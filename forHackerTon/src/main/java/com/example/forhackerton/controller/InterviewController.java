d39380cfd03d682623da66b8db764815950aee77
jinwooshin <80630445+goldadam@users.noreply.github.com>
2023-05-07 05:25:51 +0900

    interview changed

--- forHackerTon/src/main/java/com/example/forhackerton/controller/InterviewController.java
+++ forHackerTon/src/main/java/com/example/forhackerton/controller/InterviewController.java
@@ -30,17 +30,21 @@ public class InterviewController {
     private final List<ChatMessage> messages = new ArrayList<>();
     Logger logger = LoggerFactory.getLogger(InterviewController.class);
     boolean flag = false;
+    int t = 0;
     @ResponseBody
     @PostMapping("/makeChat")
     public BaseResponse chatWithMe(@RequestBody InterviewRequestDto requestDto){
         try{
+            ChatMessage systemMessage;
             OpenAiService service = new OpenAiService(token);
-            if(!flag){
-                ChatMessage systemMessage = new ChatMessage(
+            if(!flag && t == 0){
+                systemMessage = new ChatMessage(
                         ChatMessageRole.SYSTEM.value(), "너는 사용자와 대화하는 동안 면접관 역할을 한다. 사용자는 이 회사에 입사하고 싶어하는 상황이고, 사용자의 대화를 바탕으로 면접을 진행하면 된다.\n" +
                         "기존에 했던 질문과 유사한 질문들은 하지 않고, 사용자의 대답을 바탕으로 관련된 새로운 질문들을 하는 방식으로 면접을 진행하며, " +
                         "한번에 한가지 질문만 한다." + "너는 " + requestDto.getTmp() + "의 성격으로 대답해주면 돼.");
                 flag = true;
+                messages.add(systemMessage);
+                t++;
             }
             String k = requestDto.getQuestion();
             messages.add(new ChatMessage(ChatMessageRole.USER.value(), k));
