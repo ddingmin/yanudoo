3bb9c246b373b04f0a3d8b3ea221490d8e0d9b58
jinwooshin <80630445+goldadam@users.noreply.github.com>
2023-05-07 00:56:24 +0900

    interviewController changed

--- forHackerTon/src/main/java/com/example/forhackerton/controller/InterviewController.java
+++ forHackerTon/src/main/java/com/example/forhackerton/controller/InterviewController.java
@@ -3,6 +3,8 @@ package com.example.forhackerton.controller;
 
 import com.example.forhackerton.common.BaseResponse;
 import com.example.forhackerton.config.RegularResponseStatus;
+import com.example.forhackerton.data.ChatGptResponseDto;
+import com.example.forhackerton.data.InterviewRequestDto;
 import com.example.forhackerton.data.QuestionRequestDto;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
@@ -27,32 +29,41 @@ public class InterviewController {
 
     private final List<ChatMessage> messages = new ArrayList<>();
     Logger logger = LoggerFactory.getLogger(InterviewController.class);
-
+    boolean flag = false;
     @ResponseBody
     @PostMapping("/makeChat")
-    public BaseResponse chatWithMe(@RequestBody QuestionRequestDto requestDto){
+    public BaseResponse chatWithMe(@RequestBody InterviewRequestDto requestDto){
+        try{
+            OpenAiService service = new OpenAiService(token);
+            if(!flag){
+                ChatMessage systemMessage = new ChatMessage(
+                        ChatMessageRole.SYSTEM.value(), "너는 사용자와 대화하는 동안 면접관 역할을 한다. 사용자는 이 회사에 입사하고 싶어하는 상황이고, 사용자의 대화를 바탕으로 면접을 진행하면 된다.\n" +
+                        "기존에 했던 질문과 유사한 질문들은 하지 않고, 사용자의 대답을 바탕으로 관련된 새로운 질문들을 하는 방식으로 면접을 진행하며, " +
+                        "한번에 한가지 질문만 한다." + "너는 " + requestDto.getTmp() + "의 성격으로 대답해주면 돼.");
+                flag = true;
+            }
+            String k = requestDto.getQuestion();
+            messages.add(new ChatMessage(ChatMessageRole.USER.value(), k));
 
-        OpenAiService service = new OpenAiService(token);
-        ChatMessage systemMessage = new ChatMessage(
-                ChatMessageRole.SYSTEM.value(), "면접봇");
-        String k = requestDto.getQuestion();
-        messages.add(new ChatMessage(ChatMessageRole.USER.value(), k));
-
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .messages(messages)
+            ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+                    .builder()
+                    .model("gpt-3.5-turbo")
+                    .messages(messages)
 //                .n(1)
-                .maxTokens(700)
-                .temperature(0.0)
+                    .maxTokens(700)
+                    .temperature(0.0)
 //                .logitBias(new HashMap<>())
-                .build();
-        String answer = service.createChatCompletion(chatCompletionRequest).getChoices().get(0)
-                .getMessage().getContent();
-        logger.info(answer);
-        messages.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), answer));
+                    .build();
+            String answer = service.createChatCompletion(chatCompletionRequest).getChoices().get(0)
+                    .getMessage().getContent();
+            logger.info(answer);
+            messages.add(new ChatMessage(ChatMessageRole.ASSISTANT.value(), answer));
 
-        return new BaseResponse(RegularResponseStatus.OK.getCode(), answer, RegularResponseStatus.OK.getMessage());
+            return new BaseResponse(RegularResponseStatus.OK.getCode(), answer, RegularResponseStatus.OK.getMessage());
+        }catch (Exception e){
+            e.printStackTrace();
+            return new BaseResponse<>(RegularResponseStatus.SERVICE_UNAVAILABLE.getCode(), ChatGptResponseDto.builder().build(), RegularResponseStatus.SERVICE_UNAVAILABLE.getMessage());
+        }
     }
 
 }
---
+++ forHackerTon/src/main/java/com/example/forhackerton/data/InterviewRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.forhackerton.data;
+
+import lombok.Data;
+
+@Data
+public class InterviewRequestDto {
+    private String tmp;
+    private String question;
+}
